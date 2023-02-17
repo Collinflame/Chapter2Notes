d3aca81ee17e7a3116751b02d9825068bbd7dc8b
3siy <3siy@fresnou.org>
2023-02-17 14:16:13 -0800

    while loops updated

---
+++ WhileLoops1.js
@@ -0,0 +1,88 @@
+const readline = require('readline-sync');
+
+//Question 1
+// let x=0;
+// while (x < 26){
+//     console.log(x);
+//     x++;
+// }
+
+//Question 2
+// let x=0;
+// while (x < 51){
+//     console.log(x);
+//     x++;
+// }
+
+//Question 3
+// let x=-10;
+// while (x < 11){
+//     console.log(x);
+//     x++;
+// }
+
+//Question 4
+// let x=0;
+// while (x > -51){
+//     console.log(x);
+//     x--;
+// }
+
+// Question 5
+// let start=0;
+// while (start < 101){
+//     if (start % 2 == 0){
+//         console.log(start);
+//     }
+//     start++;
+// }
+
+//Question 6
+// let start=100;
+// while (start < 101){
+//     if (start % 2 == 1){
+//         console.log(start);
+//     }
+//     start--;
+// }
+
+//Question 7
+// let start=0;
+// while (start < 101){
+//     if (start % 3 == 0){
+//         console.log(start);
+//     }
+//     start++;
+// }
+
+//Question 8
+// let start=0;
+// while (start < 1001){
+//     if (start % 5 == 0){
+//         console.log(start);
+//     }
+//     start++;
+// }
+
+//Question 9
+// let start=0;
+// while (start <= 100){
+//     if (start < 10){
+//         console.log(start);
+//     } else if (start > 25) {
+//         console.log(start);
+//     }
+//     start++;
+// }
+
+//Question 10
+let start=0;
+while (start <= 100){
+    if (start < 10 && start > 20){
+        console.log(start);
+    } else if (start < 40 && start > 50) {
+        console.log(start);
+    }
+    start++;
+}
+// && doesn't work
