ffed25078060833e5aa2cf749f736eb5a8a8fe29
3siy <3siy@fresnou.org>
2023-02-24 13:32:10 -0800

    for loops

---
+++ ForLoops1.js
@@ -0,0 +1,75 @@
+const readline = require('readline-sync');
+
+//Question 1
+// for (let i = 0; i <= 150; i++){
+//     console.log(i);
+// }
+
+//Question 2
+// for (let i = 0; i <= 40; i++){
+// console.log(i * i);
+// }
+
+//Question 3
+// for (let i = 0; i <= 40; i++){
+//     console.log(i * i * i);
+// }
+
+//Question 4
+// for (let i = 0; i <= 700; i++){
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//Question 5
+// for (let i = 350; i <= 1000; i++){
+//     if (i % 10 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//Question 6
+// let sum = 0;
+// for (let i = 0; i <= 50; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//Question 7
+// let sum = 0;
+// for (let i = 35; i <= 70; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//Question 8
+// let sum = 1;
+// for (let i = 1; i <= 15; i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+//Question 9
+// let sum = 1;
+// for (let i = 10; i <= 35; i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+//Question 10
+// for (let i = 0; i < 12; i++){
+//     console.log("Happy Holidays!");
+// }
+
+//Question 11
+// let holidaynum = Number (readline.question ("Enter The Amount of Holiday Messages You Want to Receive:"));
+// for (let i = 0; i < holidaynum; i++){
+//     console.log("Happy Holidays!");
+// }
+
+//Question 12
+// let holidaynum = Number (readline.question ("Enter The Amount of Holiday Messages You Want to Receive:"));
+// for (let i = 0; i < holidaynum; i++){
+//     console.log("Happy Holidays!");
+// }
